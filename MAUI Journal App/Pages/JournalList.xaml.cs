3f41dab65d837c256bab512ef76e20e810151c91
L65FREAD <l65.fread@gmail.com>
2023-10-28 00:16:38 -0500

    Done

--- MAUI Journal App/Pages/JournalList.xaml.cs
+++ MAUI Journal App/Pages/JournalList.xaml.cs
@@ -1,10 +1,26 @@
-namespace MAUI_Journal_App.Pages;
+using System;
+using Microsoft.Maui.Controls;
 
-public partial class JournalList : ContentPage
+namespace MAUI_Journal_App.Pages
 {
-	public JournalList()
-	{
-		InitializeComponent();
-		lstJournalList.ItemsSource = App.journals;
-	}
+    public partial class JournalList : ContentPage
+    {
+        public JournalList()
+        {
+            InitializeComponent();
+
+            lstJournalList.ItemsSource = App.journals;
+
+            lstJournalList.ItemSelected += OnItemSelected;
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                
+                lstJournalList.SelectedItem = null;
+            }
+        }
+    }
 }
